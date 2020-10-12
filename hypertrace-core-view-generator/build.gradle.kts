38b8863f3d419502f07891407c422d52a06ec777
Tim Mwangi <44785807+tim-mwangi@users.noreply.github.com>
2020-10-12 08:33:21 -0700

    chore: update view framework dependency to fix snyk (#9)

--- hypertrace-core-view-generator/build.gradle.kts
+++ hypertrace-core-view-generator/build.gradle.kts
@@ -16,7 +16,7 @@ tasks.test {
 
 dependencies {
   implementation(project(":hypertrace-core-view-generator-api"))
-  implementation("org.hypertrace.core.viewgenerator:view-generator-framework:0.1.14")
+  implementation("org.hypertrace.core.viewgenerator:view-generator-framework:0.1.16")
   implementation("org.hypertrace.core.datamodel:data-model:0.1.9")
   implementation("org.hypertrace.core.spannormalizer:raw-span-constants:0.1.22")
 
