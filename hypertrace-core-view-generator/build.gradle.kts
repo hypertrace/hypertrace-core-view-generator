0239d67a355f01f6061182e41700cf74c72c0d53
Tim Mwangi <tim@traceable.ai>
2020-11-25 08:30:37 -0800

    Had to add the ignore to .snyk and update data-model as well to fix guava vulnerability

--- hypertrace-core-view-generator/build.gradle.kts
+++ hypertrace-core-view-generator/build.gradle.kts
@@ -17,7 +17,7 @@ tasks.test {
 dependencies {
   implementation(project(":hypertrace-core-view-generator-api"))
   implementation("org.hypertrace.core.viewgenerator:view-generator-framework:0.1.18")
-  implementation("org.hypertrace.core.datamodel:data-model:0.1.9")
+  implementation("org.hypertrace.core.datamodel:data-model:0.1.11")
   implementation("org.hypertrace.core.spannormalizer:raw-span-constants:0.1.22")
 
   implementation("com.google.code.findbugs:jsr305:3.0.2")
